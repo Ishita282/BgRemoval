ed917448bc34c75ebd1c6fc85133f51347396e51
Ishita282 <littleminiishita@gmail.com>
2026-04-19 14:43:59 +0530

    updated

--- bg-backend/server.js
+++ bg-backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://bg-removal-topaz.vercel.app",
   credentials: true
 }));
 
